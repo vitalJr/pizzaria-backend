e9c4f7d270da447b882ed23dba9122340a2ce845
vitalJr <vital_junior_@hotmail.com>
2025-04-21 13:05:26 +0100

    test conection

--- src/database.ts
+++ src/database.ts
@@ -7,10 +7,7 @@ class DataBase {
 
   init() {
     // mongoose.connect('mongodb://localhost/pizzaria', {});
-    mongoose.connect(
-      "mongodb://atlas-sql-68062f3e9a95173a1eda5e95-wneoi.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin",
-      {}
-    );
+    mongoose.connect("mongodb+srv://pizzaria-backend.eckccpe.mongodb.net/", {});
     console.log("DataBase connected");
   }
 }
